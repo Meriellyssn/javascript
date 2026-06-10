f7794b9ba347949186b3f80e4d1f366fe1c68078
Meirielli Sousa <merielly.ssousa@gmail.com>
2026-06-10 11:49:20 -0300

    Aula 11 - Modulo D
    estudando condicionais simples e compostas(parte - 1)

---
+++ MODULO-D/Aula11/ex008.js
@@ -0,0 +1,6 @@
+let velocidade = 60.5
+console.log(`A velocidade so seu carro é de ${velocidade} Km/h`)
+if (velocidade > 60) { // Condição Simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija com cuidado!`)
